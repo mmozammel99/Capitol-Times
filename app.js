7a41a070b490e7e4aa3c9b2549aafd3819bb0112
mmozammel99 <mhfaryanemon@gmail.com>
2022-09-03 15:37:36 +0600

    news coumte

--- app.js
+++ app.js
@@ -31,5 +31,97 @@ const loadedNews = async (category_id) => {
 
 
 
+const newsField = (newses) => {
+    const newsField = document.getElementById('news-field');
+    newsField.textContent = '';
+ 
+
+    document.getElementById('found-news').innerText = newses.length;
+    // news count
+    const notFound = document.getElementById('not-found-msg')
+    // not found msg 
+    if (newses.length === 0) {
+        notFound.classList.remove('d-none');
+        toggleSpend(false);
+        return;
+    }
+    else {
+        notFound.classList.add('d-none');
+    }
+    // news card 
+    for (const news of newses) {
+        const { image_url, title, details, author } = news;
+        const neon = news.total_view;
+         const arrayneon = [];
+          arrayneon.push(neon)
+        function test (){
+            const arr =[];
+            const ao = arrayneon[0]
+              for(let i=0; i<=26; i++){
+                  ao.push(i);
+                  arr.push(ao)
+                }
+                console.log(arr);
+      }
+        
+        console.log(arrayneon);
+        const newsCard = document.createElement('div');
+        newsCard.classList.add('card');
+        newsCard.classList.add('my-2');
+        newsCard.innerHTML = `
+        <div class="row">
+        <div class="col-md-4 p-1">
+            <img src="${image_url}" class="img-fluid rounded-start" style="width: auto; height:250px ;"alt="">
+        </div>
+        <div class="col-md-8 p-3">
+                <h5 class="card-title">${title}</h5>
+                <p class="card-text">${details.length > 350 ? details.slice(0, 350) + ' ...' : title}</p>
+            <div class="card-body align-items-center">
+                <div class="card-text row ">
+                   <div class="col-5 col-md-4 d-flex ">
+                    <div class="col-4 col-md-2">
+                        <img class="rounded-circle" style="width: 30px; height:auto ;" src="${author.img}"
+                         alt="">
+                    </div>
+                    <div class="col-8 col-md-10 ">
+                        <p>${author.name !== null? author.name :'No Data Available'  } </p>
+                    </div>
+                    
+                   </div>
+                   <div class="col-5 col-md-6 ps-5 "><img class="me-3" style="width: auto; height: 20px;" src="eye.svg"
+                   alt="">${news.total_view === null  ? 'No Data Available' : news.total_view }</div>
+                   <div class="col-2 "><button class="btn border-danger"><img style="width: auto; height: 20px;" src="right-arrows.png" alt=""></button></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    `;
+        newsField.appendChild(newsCard)
+    }
+    toggleSpend(false);
+}
+
+
+
+const selectCategory = id => {
+    toggleSpend(true);
+    loadedNews(id);
+};
+
+
+
+// lodging
+
+const toggleSpend = isLodging => {
+    const lodging = document.getElementById('lodging')
+    if (isLodging) {
+        lodging.classList.remove('d-none')
+    }
+    else {
+        lodging.classList.add('d-none')
+    }
+}
+
+
 loadedCategory()
 loadedNews('08')
