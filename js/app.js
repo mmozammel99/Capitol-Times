a68027af86ba0ce37b0477f152caf3495e70350a
mmozammel99 <mhfaryanemon@gmail.com>
2022-09-04 01:47:57 +0600

    final 2.0

--- js/app.js
+++ js/app.js
@@ -15,7 +15,6 @@ const loadedCategory = async () => {
 
 const displayCategory = (catagories) => {
     const categoryList = document.getElementById('category');
-    document.getElementById('category-name').innerText = catagories.category_name;
     catagories.forEach(category => {
         const categoryUl = document.createElement('li');
         categoryUl.classList.add('nav-item')
@@ -78,28 +77,28 @@ const newsField = (newses) => {
         newsCard.classList.add('my-2');
         newsCard.innerHTML = `
         <div class="row rounded ">
-        <div class="col-md-3 p-md-1">
+        <div class="col-md-3 p-1">
             <img src="${thumbnail_url}" class=" w-100 rounded-start" alt="">
         </div>
-        <div class="col-md-9  p-4">
+        <div class="col-md-9  px-4 d-flex flex-column">
+                <div class="card-body col-12">
                 <h4 class="card-title">${title}</h4>
                 <p class="card-text">${details.length > 700 ? details.slice(0, 700) + ' ...' : details}</p>
-            <div class="card-body align-items-center">
-                <div class="card-text row ">
-                   <div class="col-5 col-md-4 d-flex ">
-                    <div class="col-4 col-md-2">
-                        <img class="rounded-circle" style="width: 30px; height:auto ;" src="${author.img}"
+                </div>
+          
+                <div class="card-text row d-flex card-footer  ">
+                    <div class="col-4 d-flex pt-3">
+                        <img class="rounded-circle pe-2" style="width: auto; height:30px ;" src="${author.img}"
                          alt="">
-                    </div>
-                    <div class="col-8 col-md-10 ">
                         <p>${author.name !== null ? author.name : 'No Data Available'} </p>
                     </div>
                     
-                   </div>
-                   <div class="col-5 col-md-6 ps-5 "><i class="fa-sharp fa-solid fa-eye me-3"></i>${news.total_view === null ? 'No Data Available' : news.total_view}</div>
-                   <div class="col-2 "><button class="btn btn-danger " id="btn-details" data-bs-toggle="modal" data-bs-target="#staticBackdrop" onclick="selectModal('${news._id}')"><i class="fa-solid fa-circle-right fa-lg"></i></button></div>
+                
+                   <div class="col-4"><i class="fa-sharp fa-solid fa-eye pt-4 me-3"></i>${news.total_view === null ? 'No Data Available' : news.total_view}</div>
+
+                   <div class="col-4 pt-3"><button class="btn btn-danger " id="btn-details" data-bs-toggle="modal" data-bs-target="#staticBackdrop" onclick="selectModal('${news._id}')"><i class="fa-solid fa-circle-right fa-lg"></i></button></div>
                 </div>
-            </div>
+        
         </div>
     </div>
     `;
